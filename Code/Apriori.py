2d483ef3ff73a5577ce4a0a1f55ba3b643e4aea6
Chris <chris.lescinskas@gmail.com>
2022-11-09 18:05:43 -0700

    initial

---
+++ Code/Apriori.py
@@ -0,0 +1,52 @@
+import efficient_apriori as ea
+import json
+import os
+MAX_FILES = 1000 # Use this to cut the data set for quicker runtime.  Each file = 1000 playlists
+
+def extract_playlists(path, search_track):
+    global MAX_FILES
+    load = []
+    playlists = []
+    save = []
+    file_count = 0
+    filenames = os.listdir(path) #From MPD
+    for filename in sorted(filenames): #From MPD
+        print(file_count)
+        if file_count == MAX_FILES: #Stop at MAX_FILES
+            break
+        if filename.startswith("mpd.slice.") and filename.endswith(".json"): #From MPD
+            fullpath = os.sep.join((path, filename)) #From MPD
+            f = open(fullpath) #From MPD
+            js = f.read() #From MPD
+            f.close() #From MPD
+            mpd_slice = json.loads(js) #From MPD
+            for playlist in mpd_slice["playlists"]:
+                for track in playlist["tracks"]:
+                    if track["track_name"] == search_track: # If track = search track, save playlist
+                        save.append(playlist["tracks"])
+                    else:
+                        continue
+            for playlist in save:
+                load.clear()
+                for track in playlist: #Skip over search track, don't need this in output
+                    if track['track_name'] == search_track:
+                        continue
+                    else: #Build tuple from list of tracks
+                        load.append(track['track_name'])
+                playlists.append(tuple(load)) #save tuple to master list
+        file_count += 1
+    return playlists #return master list
+
+def gen_itemset(playlists):
+    itemsets = ea.itemsets_from_transactions(transactions= playlists, min_support=.2, max_length=5)
+    return itemsets
+
+def gen_rules(playlists):
+    rules = ea.generate_rules_apriori(playlists, min_confidence= .2)
+    return rules
+
+def gen_rules_items(playlists):
+    output = ea.apriori(playlists, min_support= .2, min_confidence= .2, max_length=5)
+    return output
+
+
