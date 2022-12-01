16d64b8a417a18264e90c73cc1596c64b4c364d8
Chris <chris.lescinskas@gmail.com>
2022-12-01 10:48:58 -0700

    create submission

--- Code/Apriori.py
+++ Code/Apriori.py
@@ -2,119 +2,118 @@ import csv
 import efficient_apriori as ea
 import json
 import os
-MAX_FILES = 5 # Use this to cut the data set for quicker runtime.  Each file = 1000 playlists
+MAX_FILES = 50 # Use this to cut the data set for quicker runtime.  Each file = 1000 playlists
 OUTPUT_COUNT = 1
 
 # Usage, run process_playlists_single_seed or process playlist and provide relevant pathways
-
 
 def process_playlists_single_seed(data_path, challenge_path, seed_num):  # Run on a single challenge dataset
     return_playlists = []
     track_loader = []
     playlist_count = 0
-    seeds = read_seed_playlists(challenge_path, seed_num)  # extract seed playlists
+    seeds, pid_set = read_seed_playlists(challenge_path, seed_num) # extract seed playlists and IDs
     for playlist in seeds:
-        print(playlist_count)  # print progress
+        sensitivity_offset = 0
+        file_offset = 0
         track_loader.clear()
-        for song in playlist:
-            MPD_playlists = extract_playlists(data_path, song)  # extract playlist matches
-            cands = try_support(MPD_playlists)  # run a priori until matches found
-            try:  # if no matches found, skip
-                top_cand = find_top_cand(cands)
-            except IndexError:
-                break
-            print("top cand of song:", song)
-            print(top_cand)
-            for song in top_cand:  # add top cand set to playlist, skip any tracks already added
-                if song not in track_loader:
-                    track_loader.append(song)
+        track_loader.append(pid_set[playlist_count])
+        print("Mining for playlist number: ", playlist_count)  # print progress
+        while(len(track_loader) < 501):
+            length_begin = len(track_loader)
+            for song in playlist:
+
+                if(len(track_loader) == 501):
+                    break
+                print("Number of songs in generated playlist: ", len(track_loader) - 1)
+                MPD_playlists = extract_playlists(data_path, song, file_offset)  # extract playlist matches
+                cands = try_support(MPD_playlists, sensitivity_offset)  # run a priori until matches found
+
+                try:  # if no matches found, skip
+                    extraxted_cands = extract_cands(cands)
+                except IndexError:
+                    break
+
+                for song in extraxted_cands:  # add top cand set to playlist, skip any tracks already added
+                    if len(track_loader) >= 501:
+                        break
+                    if song not in track_loader:
+                        track_loader.append(song)
+            if length_begin == len(track_loader):
+                file_offset +=MAX_FILES
+            sensitivity_offset +=1
         return_playlists.append(tuple(track_loader))  # final set of playlists
         print("completed playlist:")
         print(track_loader)
-        convert_csv(track_loader)
         playlist_count += 1
+    convert_csv(return_playlists)
     return return_playlists
 
 
-def process_playlists(data_path, challenge_path):  # run on full challenge dataset
-    return_playlists = []
-    track_loader = []
-    for i in range(9):
-        print("Seed num")
-        print(i)
-        seeds = read_seed_playlists(challenge_path, i)
-        for playlist in seeds:
-            track_loader.clear()
-            for song in playlist:
-                MPD_playlists = extract_playlists(data_path, song)
-                cands = try_support(MPD_playlists)
-                try:
-                    top_cand = find_top_cand(cands)
-                except IndexError:
-                    break
-                print("top cand")
-                print(top_cand)
-                for song in top_cand:
-                    if song not in track_loader:
-                        track_loader.append(song)
-            return_playlists.append(tuple(track_loader))
-            print("loader")
-            print(track_loader)
-            convert_csv(track_loader)
-    return return_playlists
-
-
-def extract_playlists(path, search_track):
+def extract_playlists(path, search_track, file_offset):
     global MAX_FILES
     track_loader = []
     extracted_playlists = []
     playlist_matches = []
+    playlist_count = 0
     file_count = 0
     filenames = os.listdir(path)  # From MPD
     for filename in sorted(filenames):  # from MPD
-        if file_count == MAX_FILES: # Stop at MAX_FILES
+        if file_count == MAX_FILES + file_offset: # Stop at MAX_FILES
             break
-        if filename.startswith("mpd.slice.") and filename.endswith(".json"):  # From MPD
+        if file_count < file_offset:
+            file_count += 1
+            continue
+        elif filename.startswith("mpd.slice.") and filename.endswith(".json"):  # From MPD
             fullpath = os.sep.join((path, filename))  # From MPD
             f = open(fullpath)  # From MPD
             js = f.read()  # From MPD
             f.close()  # From MPD
             mpd_slice = json.loads(js)  # From MPD
             for playlist in mpd_slice["playlists"]:
+                #if playlist_count == 100:
+                    #break
                 for track in playlist["tracks"]:
-                    if track["track_name"] == search_track: # If track = search track, save playlist
+                    if track["track_uri"] == search_track: # If track = search track, save playlist
+                        playlist_count += 1
                         playlist_matches.append(playlist["tracks"])
                     else:
                         continue
+
             for playlist in playlist_matches:
+                extracted_playlist_max_size = 0
                 track_loader.clear()
                 for track in playlist:  # Skip over search track, don't need this in output
-                    if track['track_name'] == search_track:
+                    if track['track_uri'] == search_track:
                         continue
+                    #if extracted_playlist_max_size == 75:
+                        #break
                     else:  # Build tuple from list of tracks
-                        track_loader.append(track['track_name'])
+                        track_loader.append(track['track_uri'])
+                        extracted_playlist_max_size += 1
                 extracted_playlists.append(tuple(track_loader))  # save tuple to master list
         file_count += 1
+    print("Playlists analyzed for song: ", search_track, playlist_count)
     return extracted_playlists  # return master list
 
 
 def gen_itemset(playlists, support):
-    itemsets = ea.itemsets_from_transactions(transactions= playlists, min_support=support, max_length=3 )
+    itemsets = ea.itemsets_from_transactions(transactions= playlists, min_support=support, max_length=2 )
     return itemsets
 
 
-def gen_rules(playlists):
+def gen_rules(playlists):  # NOT IN USE
     rules = ea.generate_rules_apriori(playlists, min_confidence= .1)
     return rules
 
 
-def gen_rules_items(playlists):
-    output = ea.apriori(playlists, min_support= .2, min_confidence= .2, max_length=5)
+def gen_rules_items(playlists):  # NOT IN USE
+    output = ea.apriori(playlists, min_support=.2, min_confidence=.2, max_length=3)
     return output
 
 
-def read_seed_playlists(path, seed_num): #seed_num is Challenge # from challenge data set
+def read_seed_playlists(path, seed_num):  # seed_num is Challenge # from challenge data set
     filename = os.listdir(path)
+    pid_set = [] # playlist IDs to be included in output
     start, end, counter = 0, 0, 0
     return_playlists, tuple_loader = [], []
     for file in filename:
@@ -127,9 +126,11 @@ def read_seed_playlists(path, seed_num): #seed_num is Challenge # from challenge
             if seed_num == 0:
                 end = 999
                 for playlist in seeds["playlists"]:
+
                     if counter > end:
                         break
                     else:
+                        pid_set.append(playlist["pid"])
                         for track in playlist["tracks"]:
                             tuple_loader.append(track["track_name"])
                         return_playlists.append(tuple(tuple_loader))
@@ -139,6 +140,7 @@ def read_seed_playlists(path, seed_num): #seed_num is Challenge # from challenge
                 end = ((seed_num+1)*1000)-1
 
                 for playlist in seeds["playlists"]:
+
                     tuple_loader.clear()
                     if counter < start:
                         counter +=1
@@ -146,27 +148,25 @@ def read_seed_playlists(path, seed_num): #seed_num is Challenge # from challenge
                     elif counter > end:
                         break
                     else:
+                        pid_set.append(playlist["pid"])
                         for track in playlist["tracks"]:
-                            tuple_loader.append(track["track_name"])
+                            tuple_loader.append(track["track_uri"])
                         return_playlists.append(tuple(tuple_loader))
                         counter += 1
-            return return_playlists
-    return
+    return return_playlists, pid_set
 
 
-def try_support(playlists):
-    for i in range(7):  # try different support until a candidate set is found
-        try:
-            cand = gen_itemset(playlists, (.35/(i+1)))
-        except IndexError:
-            continue
-        if len(cand) > 2:
-            break
+def try_support(playlists, offset): # try different support until a candidate set is found
+    try:
+        cand = gen_itemset(playlists, (.10/(offset+1)))
+    except IndexError:
+        return []
     return cand
 
 
-def find_top_cand(cand):  # pull out top cand from candidate set
+def find_top_cand(cand):  # pull out top cand from candidate set NOT IN USE
     sets = cand[0]
+    print(sets)
     keys = sorted(cand[0].keys())
     max_set = keys[-1]
     top_cands = sorted(sets[max_set].values())
@@ -175,12 +175,65 @@ def find_top_cand(cand):  # pull out top cand from candidate set
         if v == top_cand_val:
             return k
 
+def extract_cands(cand):  # pull out top cand from candidate set
+    return_cands = []
+    sets = cand[0]
+    for key in sets.values():
+        for song_set in key.keys():
+            for song in song_set:
+                if song not in return_cands:
+                    return_cands.append(song)
+    return return_cands
+
 
 def convert_csv(playlists):
     global OUTPUT_COUNT
-    name = "Playlist" + str(OUTPUT_COUNT)
+    name = "Playlist" + str(OUTPUT_COUNT)+ ".csv"
     with open(name, 'w') as f:
         write = csv.writer(f)
         write.writerow(playlists)
     OUTPUT_COUNT += 1
+
+def gen_dummy_playlist():
+    playlist_length = 0
+    dummy_playlist = []
+    filenames = os.listdir("./MPD_data")  # From MPD
+    for filename in sorted(filenames):
+        if filename.startswith("mpd.slice.") and filename.endswith(".json"):  # From MPD
+            fullpath = os.sep.join(("./MPD_data", filename))  # From MPD
+            f = open(fullpath)  # From MPD
+            js = f.read()  # From MPD
+            f.close()  # From MPD
+            mpd_slice = json.loads(js)  # From MPD
+        for playlist in mpd_slice["playlists"]:
+            for track in playlist["tracks"]:
+                if len(dummy_playlist) == 500:
+                    return dummy_playlist
+                track_name = track["track_uri"]
+                if track_name not in dummy_playlist:
+                    dummy_playlist.append(track_name)
+                    playlist_length += 1
+
+
+def create_submission(data_path, challenge_path):
+    final_playlists = [['team_info', 'CU Boulder Data-Mining', 'chris.lescinskas@gmail.com']]
+    dummy_playlist = gen_dummy_playlist()
+    playlist_loader = []
+    pids = []
+    for i in range(9):
+        print(final_playlists)
+        pids.clear()
+        seeds, pids = read_seed_playlists("./Challenge_data", i)
+        if i != 2:
+            for i in range(999):
+                playlist_loader.clear()
+                playlist_loader.append(pids[i])
+                for item in dummy_playlist:
+                    playlist_loader.append(item)
+                final_playlists.append(playlist_loader)
+        if i == 2:
+            mined_playlists = process_playlists_single_seed(data_path, challenge_path, i)
+            for item in mined_playlists:
+                final_playlists.append(item)
+    convert_csv(final_playlists)
 
